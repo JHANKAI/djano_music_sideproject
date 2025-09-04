d9b36d6fdc9d63f1a33e376fc9e61f9b0b7389af
Emma <qwed93fu6iop@gmail.com>
2025-09-04 17:21:25 +0800

    Add create_playlist view for creating playlists

--- music_app/views.py
+++ music_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PlaylistForm
 
 # Create your views here.
+
+def create_playlist(request):
+    if request.method == "POST":
+        form = PlaylistForm(request.POST)
+        if form.is_valid():
+            playlist = form.save(commit=False)  # 先產生 Playlist 物件但不存入資料庫
+            playlist.user = request.user # 綁定目前登入的使用者
+            playlist.save() # 真正存到資料庫
+            form.save_m2m() # 存 ManyToMany 的歌曲
+            return redirect("playlist_list")
+        
+    else:
+        form = PlaylistForm()
+
+    return render(request, "create_playlist.html", {"form": form})
